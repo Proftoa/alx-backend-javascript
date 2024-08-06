6203aa09a2c74d474195eeab87057b5e06db725f
Proftoa <ogunjobimoshoodo@gmail.com>
2024-08-06 07:13:36 -0400

    Updated

---
+++ 0x00-ES6_basic/.eslintrc.js
@@ -0,0 +1,11 @@
+{
+  "extends": ["airbnb", "airbnb/hooks"],
+  "parserOptions": {
+    "ecmaVersion": 2020,
+    "sourceType": "module"
+  },
+  "rules": {
+    // Customize your ESLint rules here
+  }
+}
+
