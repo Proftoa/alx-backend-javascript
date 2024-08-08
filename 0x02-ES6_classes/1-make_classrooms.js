ca8732b440a4012030a69205f931acf68a35844b
Proftoa <ogunjobimoshoodo@gmail.com>
2024-08-08 18:34:38 -0400

    Task 1

--- 0x02-ES6_classes/1-make_classrooms.js
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -1,6 +1,9 @@
-// eslint-disable-next-line import/extensions
-import ClassRoom from './0-classroom.js';
+import ClassRoom from './0-classroom';
 
-export default function initializeRooms() {
-  return [new ClassRoom(19), new ClassRoom(20), new ClassRoom(34)];
+function initializeRooms() {
+  const sizes = [19, 20, 34];
+  return sizes.map((size) => new ClassRoom(size));
 }
+
+export default initializeRooms;
+
